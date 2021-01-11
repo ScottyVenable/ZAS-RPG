ebe1e757a595b3c0e56244d84099933f5692e6f1
ScottyVenable <scottyvenable@gmail.com>
2021-01-11 18:54:52 -0500

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZASRPG
+{
+  public class Item
+  {
+    public int ID {get; set;}
+    public string Name {get; set;}
+    public int Weight {get; set;}
+    public string Type {get; set;}
+    public double BuyPrice {get; set;}
+    public double SellPrice {get; set;}
+  }
+}
---
+++ Player.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZASRPG
+{
+  public class Player
+  {
+    public string Name { get; set;}
+    
+    public int HP {get; set;}
+    public int Hunger {get; set;}
+    public int Thirst {get; set;}
+    
+  }
+}
